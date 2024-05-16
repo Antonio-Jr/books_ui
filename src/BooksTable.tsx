ceb864ae7c6afcb015559ba559891de04765e6e3
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2024-05-16 13:37:00 -0400

    updating configurations

--- src/BooksTable.tsx
+++ src/BooksTable.tsx
@@ -67,12 +67,6 @@ export const BooksTable = ({ entity, search }: BooksTableProps) => {
             </TableRow>
           ))}
       </TableBody>
-      {/* <TableFooter>
-      <TableRow>
-        <TableCell colSpan={3}>Total</TableCell>
-        <TableCell className="text-right">$2,500.00</TableCell>
-      </TableRow>
-    </TableFooter> */}
     </Table>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
 });
 
